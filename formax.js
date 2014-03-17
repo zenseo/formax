03e8e96fb1e0d0f2dc1076c2ff5df20c874dcf2d
vaibhavpandeyvpz <vaibhavpandey@live.in>
2014-03-17 13:21:47 +0000

    Initial push to Github
    Syncing initial code to set things up

---
+++ formax.js
@@ -0,0 +1,52 @@
+(function($) {
+	$.fn.formax = function(Options) {
+		var Defaults = $.extend({
+            method : null,
+            parameters : {},
+            response: function(Context, Response) {
+            	console.log(Context);
+            },
+            url : null
+        }, Options);
+		return this.each(function() {
+			var $Form = jQuery(this);
+			$Form.bind("submit", function(Event) {
+				Event.preventDefault();
+				var $Data = {};
+			    var $Temporary = $Form.serializeArray();
+			    $.each($Temporary, function() {
+			        if ($Data[this.name] !== undefined) {
+			            if (!$Data[this.name].push)
+			            	$Data[this.name] = [$Data[this.name]];
+			            $Data[this.name].push(this.value || '');
+			        } else
+			        	$Data[this.name] = this.value || '';
+			    });
+			    $.each(Defaults.parameters, function(Key, Value) {
+			    	if ($Data[Key] !== undefined) {
+			            if (!$Data[Key].push)
+			            	$Data[Key] = [$Data[this.name]];
+			            $Data[Key].push(Value || '');
+			        } else
+			        	$Data[Key] = Value || '';
+			    })
+				var $Method = Defaults.method != null ? Defaults.method : $Form.attr("method");
+				var $Target = Defaults.url != null ? Defaults.url : $Form.attr("action");
+				console.log($Data);
+			    jQuery.ajax({
+			    	context: $Form,
+					data: $Data,
+					error: function() {
+						Defaults.response(this, null);
+					},
+					success: function(Response) {
+						Defaults.response(this, Response);
+					},
+					type: $Method,
+					url: $Target
+				});
+				return false;
+			});
+	    });
+	}
+}(jQuery));
